ced750e59652531fd1202cc992fa3ff829da9d1e
Doug <27059467+doug-newton@users.noreply.github.com>
2024-01-17 17:49:26 +0200

    add next(), prev(), append(), prepend()

--- Test/node.cpp
+++ Test/node.cpp
@@ -5,3 +5,40 @@ TEST(Node, Constructor) {
 	Node<int> n(42);
 	EXPECT_EQ(n.getValue(), 42);
 }
+
+TEST(Node, Append) {
+	Node<int> n1(23);
+	Node<int> n2(34);
+
+	n1.append(&n2);
+
+	EXPECT_EQ(n1.next(), &n2);
+	EXPECT_EQ(n2.prev(), &n1);
+}
+
+TEST(Node, Append_Null) {
+	Node<int> n1(23);
+
+	n1.append(nullptr);
+
+	EXPECT_EQ(n1.next(), nullptr);
+}
+
+TEST(Node, Prepend) {
+	Node<int> n1(23);
+	Node<int> n2(34);
+
+	n1.prepend(&n2);
+
+	EXPECT_EQ(n2.next(), &n1);
+	EXPECT_EQ(n1.prev(), &n2);
+}
+
+TEST(Node, Prepend_Null) {
+	Node<int> n1(23);
+
+	n1.prepend(nullptr);
+
+	EXPECT_EQ(n1.prev(), nullptr);
+}
+
--- node.h
+++ node.h
@@ -8,16 +8,52 @@ public:
 	Node(T data);
 	T getValue();
 
+	Node<T>* next();
+	Node<T>* prev();
+
+	void append(Node<T>* node);
+	void prepend(Node<T>* node);
+
 private:
 
 	T m_data;
+	Node<T>* m_next;
+	Node<T>* m_prev;
 };
 
 template <typename T>
 Node<T>::Node(T data) :
-	m_data(data) { }
+	m_data(data),
+	m_next(nullptr),
+	m_prev(nullptr) { }
 
 template <typename T>
 T Node<T>::getValue() {
 	return this->m_data;
+}
+
+template <typename T>
+Node<T>* Node<T>::next() {
+	return this->m_next;
+}
+
+template <typename T>
+Node<T>* Node<T>::prev() {
+	return this->m_prev;
+}
+
+template <typename T>
+void Node<T>::append(Node<T>* node) {
+	this->m_next = node;
+	if (node != nullptr) {
+		node->m_prev = this;
+	}
+}
+
+template <typename T>
+void Node<T>::prepend(Node<T>* node) {
+	this->m_prev = node;
+	if (node != nullptr) {
+		node->m_next = this;
+	}
 }
